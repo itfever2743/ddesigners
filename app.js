a376fe7da10c6576ef1beba7f1a09fc9daa9cda3
Saifi <itf2743@gmail.com>
2022-08-23 10:44:41 +0500

    Index.html Route Changing

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(require('./router/routes'));
 
 if (process.env.NODE_ENV == 'production') {
-  app.use(express.static('client/public'));
+  app.use(express.static('client/build'));
 } else {
   app.use(express.static('client/build'));
 }
