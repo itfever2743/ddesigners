bcd1f728cfedf83d33bd575c4a4bd76299e8df9d
Saifi <itf2743@gmail.com>
2022-08-24 10:23:52 +0500

    Android app api adding

---
+++ api_data/data.js
@@ -0,0 +1,37 @@
+[
+  {
+    _id: '62ff97c03cf03cda45e339c8',
+    name: 'Saif Ullah',
+    email: 'saifi143@gmail.com',
+    phone: 3186364518,
+    work: 'Front End Developer',
+  },
+  {
+    _id: '630399c60f02ce9d51ac1cbc',
+    name: 'Saif Ullah',
+    email: 'saifi786@gmail.com',
+    phone: 3186364518,
+    work: 'Front End Developer',
+  },
+  {
+    _id: '6303a9d5a0efbd1d37bb2671',
+    name: 'Saif Ullah',
+    email: 'saifi114@gmail.com',
+    phone: 3203838417,
+    work: 'Developer',
+  },
+  {
+    _id: '6303b88144f13557d7d830f7',
+    name: 'Saif Ullah',
+    email: 'saifi183@gmail.com',
+    phone: 3186364518,
+    work: 'Developer',
+  },
+  {
+    _id: '6303b94dde2db13fb16b028f',
+    name: 'saif',
+    email: 'saifi123@gmail.com',
+    phone: 3203838417,
+    work: 'Developer',
+  },
+];
